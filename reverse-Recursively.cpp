b7c29271bf2d5dab94b7b80a26b44b3ec9dff3b3
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-02-26 16:07:38 +0530

    Add files via upload

---
+++ reverse-Recursively.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+    node* n= new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+node* reverseReccursive(node* &head){
+    
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    node* newhead=reverseReccursive(head->next);
+    
+    head->next->next=head;
+    head->next=NULL;
+    
+    return newhead;
+}
+
+node* reverse(node* head){
+    node* preptr=NULL;
+    node* curptr=head;
+    node* nextptr;
+    while(curptr!=NULL){
+        nextptr=curptr->next;
+        curptr->next=preptr;
+        
+        preptr=curptr;
+        curptr=nextptr;
+    }
+    return preptr;
+}
+void display(node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    
+    
+    node* newhead= reverseReccursive(head);
+    display(newhead);
+    
+    return 0;
+}
